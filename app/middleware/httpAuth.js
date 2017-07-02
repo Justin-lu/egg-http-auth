5a73749a135b2db0ba6d398bc7c8e6d3d98072a7
Justin <gdjyluxiaoyong@gmail.com>
2017-07-02 15:33:55 +0800

    add http auth middleware

---
+++ app/middleware/httpAuth.js
@@ -0,0 +1,33 @@
+'use strict';
+
+const assert = require('assert');
+
+module.exports = options => {
+  return function* backdoorAuth(next) {
+    const { username, password } = options;
+    assert.strictEqual(typeof username, 'string', 'Must set `app.config.httpAuth.username` when httpAuth plugin enable');
+    assert.strictEqual(typeof password, 'string', 'Must set `app.config.httpAuth.password` when httpAuth plugin enable');
+    const auth = this.request.get('authorization');
+    if (!auth) {
+      this.status = 401;
+      this.set('WWW-Authenticate', 'Basic realm="Secure Area"');
+      this.body = '<html><body>Need some creds son</body></html>';
+    } else if (auth) {
+      // Split on a space, the original auth looks like  "Basic Y2hhcmxlczoxMjM0NQ==" and we need the 2nd part
+      const tmp = auth.split(' ');
+      // create a buffer and tell it the data coming in is base64
+      const buf = new Buffer(tmp[1], 'base64');
+      // read it back out as a string
+      // At this point plainAuth = "username:password"
+      const plainAuth = buf.toString();
+      const [ u, p ] = plainAuth.split(':'); // split on a ':'
+      if ((u === username) && p === password) {
+        yield next;
+      } else {
+        this.status = 401;
+        this.set('WWW-Authenticate', 'Basic realm="Secure Area"');
+        this.body = '<html><body>You shall not pass</body></html>';
+      }
+    }
+  };
+};
